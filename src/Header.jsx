d802952b059bddd772aaf5c1ea026fd51d09a268
Santhosh D <santhosh.webd679@gmail.com>
2025-06-24 16:30:40 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,52 +1,60 @@
-import React, { useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import logoimg from "../src/images/logo.png";
-import cartimg from "./images/cart.png"
+import cartimg from "./images/cart.png";
 
-export const Header = ({cartCount,headdesign,setHeaddesign}) => {
+export const Header = ({ cartCount, headdesign, setHeaddesign }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? 'hidden' : 'auto';
+  }, [menuOpen]);
 
   return (
     <div>
       <nav className="stickyheader">
         <div className="mainheader">
           <div className="logo">
-            <img src={logoimg} alt="" />
+            <img src={logoimg} alt="Logo" />
             <h2 className='meatify'><span>M</span>EATIF<span>Y</span></h2>
           </div>
-          <ul>
-            <li onClick={() => setHeaddesign("home")}>
+
+          <div className="mobile-menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className={`bar ${menuOpen ? 'bar1-open' : ''}`}></div>
+            <div className={`bar ${menuOpen ? 'bar2-open' : ''}`}></div>
+            <div className={`bar ${menuOpen ? 'bar3-open' : ''}`}></div>
+          </div>
+
+          <ul className={`nav-links ${menuOpen ? 'nav-active' : ''}`}>
+            <li onClick={() => { setHeaddesign("home"); setMenuOpen(false); }}>
               <Link to='/'>Home {headdesign === "home" && <hr />}</Link>
             </li>
-            <Link to='/aboutuspg'>
-              <li onClick={() => setHeaddesign("aboutus")}>
-                About Us {headdesign === "aboutus" && <hr />}
-              </li>
-            </Link>
-            <Link to='/productpg'>
-              <li onClick={() => setHeaddesign("productpage")}>
-                Products {headdesign === "productpage" && <hr />}
-              </li>
-            </Link>
-            <Link to='/cartpg'>
-              <li onClick={() => setHeaddesign("cartpage")}>
+            <li onClick={() => { setHeaddesign("aboutus"); setMenuOpen(false); }}>
+              <Link to='/aboutuspg'>About Us {headdesign === "aboutus" && <hr />}</Link>
+            </li>
+            <li onClick={() => { setHeaddesign("productpage"); setMenuOpen(false); }}>
+              <Link to='/productpg'>Products {headdesign === "productpage" && <hr />}</Link>
+            </li>
+            <li onClick={() => { setHeaddesign("cartpage"); setMenuOpen(false); }}>
+              <Link to='/cartpg'>
                 <div className="cartli">
                   <div className="carttext">
                     Cart {headdesign === "cartpage" && <hr />}
                   </div>
-                  <img src={cartimg} className='cartimage' alt="" />
+                  <img src={cartimg} className='cartimage' alt="Cart" />
                   {cartCount > 0 && <span className='cartcount'>{cartCount}</span>}
                 </div>
-
-              </li>
-            </Link>
+              </Link>
+            </li>
           </ul>
-           <div className="loginbuttonsec">
-            <Link to='#'><button className='loginbutton'>Login</button> </Link>
+
+          <div className="loginbuttonsec">
+            <Link to="https://meatify-login-signup.netlify.app/"><button className='loginbutton'>Login</button></Link>
           </div>
         </div>
       </nav>
 
-      <Outlet></Outlet>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
